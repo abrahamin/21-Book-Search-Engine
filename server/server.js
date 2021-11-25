771a29853c1e02af520a6ba34598866bf8dc045e
Abraham Hong <hhmfjg@gmail.com>
2021-11-24 23:41:32 -0500

    implemented apollo server middleware

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
-const routes = require("./routes");
+// const routes = require("./routes");
+
+const { ApolloServer } = require("apollo-server-express");
+
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+const db = require("./config/connection");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,7 +38,11 @@ mongoose.connect(
   }
 );
 
-app.use(routes);
+// app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
